bb6ada1571b8cf35fd72f707034fb57a1899b498
bigwangsmash <bigwangsmash@lawn-128-61-61-174.lawn.gatech.edu>
2014-09-20 02:34:25 -0400

    font

--- RhythmSlap.spritebuilder/MainScene.swift
+++ RhythmSlap.spritebuilder/MainScene.swift
@@ -15,6 +15,9 @@ class MainScene : CCNode
     weak var face : Face?
     weak var timer: Timer?
     weak var comboBar: ComboBar?
+    weak var colorGradientNode: CCNodeGradient?
+    weak var glowNode: CCNodeColor?
+    weak var gestureMessage: CCLabelTTF?
     
     //numberOfSlaps
     var currentNumOfSlaps : Int = 5
@@ -24,7 +27,10 @@ class MainScene : CCNode
     var currentWave : Int = 0
     
     //game related Bools
+    var gameCountdownMode : Bool = true;
+    var gameCountdown: int = 4;
     var gameStarted : Bool = false
+    
     var gameEnded : Bool = false
     
     //game related values
@@ -32,10 +38,40 @@ class MainScene : CCNode
     
     func didLoadFromCCB()
     {
-        
+        self.gestureMessage.string = ""
     }
     
     override func update(delta : CCTime)
+    {
+        if (!gameStarted)
+        {
+            if (gameCountdownMode)
+            {
+                if (timer.currentTime%1 == 0)
+                {
+                    if (gameCountdown == 0)
+                    {
+                        self.gestureMessage.string = "START!"
+                        self.gameCountdownMode = false
+                        self.gameStarted = true
+                        
+                    }
+                    else if (gameCountdown < 4)
+                    {
+                        self.gestureMessage.string = "\(gameCountdown)"
+                    }
+                    self.timer.currentTime = 0
+                    self.gameCountdown--
+                }
+            }
+            else
+            {
+                
+            }
+        }
+    }
+    
+    func setPoints(points: int)
     {
         
     }
